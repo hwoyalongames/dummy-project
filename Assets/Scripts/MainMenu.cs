e39b9ad93672fc38e1fe77cc041c3ab92b7b33db
hwoyalon games <hwoyalongames@gmail.com>
2025-11-10 19:18:47 +0700

    edited main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string MainGameScene;
+
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(MainGameScene);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Application.Quit();
     }
 }
